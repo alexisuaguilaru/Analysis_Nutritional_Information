e23e3a330f38edd566d95ce99faf5e990da7efa7
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2025-06-23 17:23:43 -0600

    ADD functions for generate plot of macronutrient distributions

---
+++ StatisticalAnalysis/SourceStatisticalAnalysis/Base.py
@@ -0,0 +1,18 @@
+Carbs = 'Carbs'
+Protein = 'Protein'
+Fat = 'Fat'
+Macronutrients = [Carbs, Protein, Fat]
+
+Diet = 'Diet_type'
+Diets = ['dash', 'keto', 'mediterranean', 'paleo', 'vegan']
+
+Dash = 'dash'
+Keto = 'keto'
+Mediterranean = 'mediterranean'
+Paleo = 'paleo'
+Vegan = 'vegan'
+
+Recipe = 'Recipe_name'
+Total = 'Total_macronutrients'
+
+ColorPalette = ['green','red','gold']
---
+++ StatisticalAnalysis/SourceStatisticalAnalysis/Plots.py
@@ -0,0 +1,75 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from .Base import *
+
+import pandas as pd
+from matplotlib.figure import Figure
+from matplotlib.axes import Axes
+from typing import Iterator
+import numpy as np
+from scipy import stats 
+
+
+def PlotMacronutrients(
+        Dataset:pd.DataFrame,
+        Diet:str=None,
+    ) -> Figure:
+    """
+    Function for plotting distribution 
+    of macronutrients
+    """
+    fig , axes , config_plot = CreateMosaicPlot()
+    for macronutrient , mosaic_position , color in config_plot:
+        axes[mosaic_position].grid(True,axis='x',color='gray',lw=1.5,ls=':')
+        
+        sns.violinplot(
+            Dataset,
+            x=macronutrient,
+            ax=axes[mosaic_position],
+            color=color,
+            bw_adjust=2,
+            fill=False,
+            linewidth=2,
+            inner_kws={'box_width':15},
+        )
+
+        SetLabels(axes[mosaic_position],XLabel=macronutrient)
+
+    title = 'Distribution of Macronutrients' + (f'\nin Diet {Diet}' if Diet else '')
+    fig.suptitle(title,fontsize=20)
+    return fig
+
+def CreateMosaicPlot(
+    ) -> tuple[Figure, dict[str,Axes], Iterator[tuple[str,str,str]]]:
+    """
+    Function for creating the fig
+    for plots
+    """
+    display = "PPCC;.FF."
+
+    fig , axes = plt.subplot_mosaic(
+        display,
+        figsize=(8,6),
+        layout='tight',
+        subplot_kw={'frame_on':False,'xlim':(0-5e-2,1+5e-2)}
+    )
+    config_plot = zip(Macronutrients,['C','P','F'],ColorPalette)
+
+    return fig , axes , config_plot
+
+def SetLabels(
+        Axes:Axes,
+        Title:str=None,
+        XLabel:str=None,
+        YLabel:str=None,
+    ) -> None:
+    """
+    Function for setting labels and title 
+    of a plot
+    """
+    if Title: Axes.set_title(Title,size=20)
+    if XLabel: Axes.set_xlabel(XLabel,size=15)
+    if YLabel: Axes.set_ylabel(YLabel,size=15)
+    
+    Axes.tick_params(axis='both',labelsize=13)
